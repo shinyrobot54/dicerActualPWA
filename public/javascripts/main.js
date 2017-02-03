6f460c4519d5c55a9585bdc4a67c32831347e4ae
Gabe Meyerson <gabemeyerson@gmail.com>
2017-02-03 15:10:15 -0600

    added the main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,58 @@
-/**
- * Created by gabe on 2/3/2017.
- */
+//   @todo
+
+"use strict";
+
+class main {
+    constructor() {
+        document.getElementById("numberOfDice").addEventListener("onchange", () => {
+            let numDice;
+            const MIN_DICE = 1, MAX_DICE = 10;
+
+            while ((typeof numDice === `undefined` || isNaN(numDice)) || (numDice < MIN_DICE) || (numDice > MAX_DICE)) {
+                numDice = document.getElementById('numberOfDice');
+            }
+        })
+        ;
+
+        document.getElementById("button").addEventListener("click", () => {
+            let numDice = document.getElementById('numberOfDice').value;
+            let i = 0;
+            document.getElementById('results').innerHTML = '';
+            if (document.getElementById('a-option').checked) {
+                while (i < numDice) {
+                    document.getElementById('results').innerHTML += (Math.floor(1 + Math.random() * 4) + ', ');
+                    i++;
+                }
+            } else if (document.getElementById('b-option').checked) {
+                while (i < numDice) {
+                    document.getElementById('results').innerHTML += (Math.floor(1 + Math.random() * 6) + ', ');
+                    i++;
+                }
+            } else if (document.getElementById('c-option').checked) {
+                while (i < numDice) {
+                    document.getElementById('results').innerHTML += (Math.floor(1 + Math.random() * 8) + ', ');
+                    i++;
+                }
+            } else if (document.getElementById('d-option').checked) {
+                while (i < numDice) {
+                    document.getElementById('results').innerHTML += (Math.floor(1 + Math.random() * 12) + ', ');
+                    i++;
+                }
+            } else if (document.getElementById('e-option').checked) {
+                while (i < numDice) {
+                    document.getElementById('results').innerHTML += (Math.floor(1 + Math.random() * 20) + ', ');
+                    i++;
+                }
+            }
+        })
+        ;
+
+    }
+
+
+}
+
+window.addEventListener('load', () => {
+    new main();
+})
+;
